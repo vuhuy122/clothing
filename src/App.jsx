a4b2c3cdec17ca3d99a84fbf41e21a64da2de69a
Phuoc.Vu <vuhuy122@gmail.com>
2023-04-01 19:48:18 +0700

    add  router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './router/home/home.component';
-
+import Navigation from './router/navigation/navigation.component';
 
 const App = () => {
+
+
   const Shop = () => {
     return (<h1>Shop</h1>)
   }
   return (
     <Routes>
-      <Route path='/home' element={<Home />} >
+      <Route path='/' element={<Navigation />} >
+        <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
